e15777d67d13eb7200826dcc7d709acb943b8558
supi008 <146051997+supi008@users.noreply.github.com>
2024-04-01 12:02:12 +0800

    01 april2024

---
+++ struktur data supiana_23241040/meet2.py
@@ -0,0 +1,22 @@
+#belajar variabel 
+# assigment 
+x = 3
+y = 9
+uang = 1000
+
+# pemanggilan 
+print ("nilai uas saya =",y)
+print ("nilai uas saya =",y)
+print ("nilai uang saya =",uang)
+print ("nilai uas saya")
+
+# penamaan variabel 
+nilai_a =9 # tidak boleh spasi 
+kotak10 = 10 # tidak boleh diawali angka  
+
+# input variabel kedua 
+x = 30
+print ("nilai uas saya =",x)
+
+
+
